b0e22723a607a2e82a37b07b8897c039608b1867
Kirill <temnodush@vk.com>
2025-03-15 14:05:11 +0500

    Добавлены docstrings к функциям.

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,8 @@
 def filter_by_state(list_dict: list[dict], state: str = "EXECUTED") -> list[dict]:
+    """
+    Функция принимает список словарей и возвращает новый список со словарями
+    у которых ключ state равен значению EXECUTED.
+    """
     new_list = []
     for i in list_dict:
         if i["state"] == state:
@@ -7,4 +11,8 @@ def filter_by_state(list_dict: list[dict], state: str = "EXECUTED") -> list[dict
 
 
 def sort_by_date(list_dict: list[dict], reverse_sort: bool = True) -> list[dict]:
+    """
+    Функция сортирует принимаемый список словарей и возвращает
+    отсортированный в порядке убывания.
+    """
     return sorted(list_dict, key=lambda date: date.get("date", 0), reverse=reverse_sort)
