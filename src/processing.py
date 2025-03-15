e731750ea524f9f1988e922d51fe5b7d5a1d3b5d
Kirill <temnodush@vk.com>
2025-03-15 13:25:40 +0500

    Добавлена новая функция которая сортирует список по значению date.

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,15 @@
-def filter_by_state(list_dict, state="EXECUTED"):
-     new_list = []
-     for i in list_dict:
-         if i["state"] == state:
-             new_list.append(i)
-     return new_list
+from typing import Dict, List
+
+
+def filter_by_state(list_dict: list[dict], state: str = "EXECUTED") -> list[dict]:
+    new_list = []
+    for i in list_dict:
+        if i["state"] == state:
+            new_list.append(i)
+    return new_list
+
+
+def sort_by_date(list_dict: list[dict], reverse_sort: bool = True) -> list[dict]:
+    return sorted(list_dict, key=lambda date: date.get("date", 0), reverse=reverse_sort)
+
 
