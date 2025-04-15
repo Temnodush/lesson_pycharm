d5c788c729b8a00de28ccb05f9113cb6f041976e
Kirill <temnodush@vk.com>
2025-04-16 00:54:13 +0500

    Реализовано логирование

--- src/masks.py
+++ src/masks.py
@@ -1,20 +1,51 @@
+import logging
+import os
+from pathlib import Path
+
+file_dir = os.path.dirname(os.path.abspath(__file__))
+log_dir = Path(file_dir).parent / "logs"
+log_dir.mkdir(parents=True, exist_ok=True)
+file_path = log_dir / "masks.log"
+
+# Настройка file_formatter
+file_formatter = logging.Formatter("%(asctime)s %(filename)s %(levelname)s: %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+
+# Настройка file_handler
+file_handler = logging.FileHandler(file_path, mode="w", encoding="utf-8")
+file_handler.setFormatter(file_formatter)
+
+logger = logging.getLogger("masks")
+logger.setLevel(logging.DEBUG)
+logger.addHandler(file_handler)
+
+
 def get_mask_card_number(card_number: str) -> str:
     """Функция, которая позволяет шифровать номер карты в формате "1234 12** **** 1234" """
     if not card_number.strip():
+        logger.error("Пользователь ввёл некорректное значение.")
         raise ValueError("Необходимо ввести номер")
     if len(card_number) != 16:
+        logger.error("Пользователь ввёл некорректную длину номера карты.")
         raise ValueError("Карта должна состоять ровно из 16 цифр.")
     if not card_number.isdigit():
+        logger.error("Пользователь ввёл не цифровое значение.")
         raise ValueError("Номер должен состоять только из цифр.")
+    logger.info("Номер карты успешно зашифрован.")
     return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[12:]}"
 
 
 def get_mask_account(card_number: str) -> str:
     """Функция, которая выводит 4 последние цифры карты и шифрует две предыдущие."""
     if card_number == "" or card_number == " ":
+        logger.error("Пользователь ввёл некорректное значение.")
         raise ValueError("Необходимо ввести номер")
     elif len(card_number) != 16:
+        logger.error("Пользователь ввёл некорректную длину номера карты.")
         raise ValueError("Карта должна состоять ровно из 16 цифр.")
     elif not card_number.isdigit():
+        logger.error("Пользователь ввёл не цифровое значение.")
         raise ValueError("Номер должен состоять только из цифр.")
+    logger.info("Номер карты успешно зашифрован.")
     return f"XX{card_number[12:]}"
+
+
--- src/utils.py
+++ src/utils.py
@@ -1,25 +1,45 @@
 import json
+import logging
 import os
+from pathlib import Path
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
-file_path = os.path.join(script_dir, "../data/operations.json")
+file_dir = os.path.dirname(os.path.abspath(__file__))
+log_dir = Path(file_dir).parent / "logs"
+log_dir.mkdir(parents=True, exist_ok=True)
+file_path = log_dir / "utils.log"
+
+# Настройка file_formatter
+file_formatter = logging.Formatter("%(asctime)s %(filename)s %(levelname)s: %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+
+# Настройка file_handler
+file_handler = logging.FileHandler(file_path, mode="w", encoding="utf-8")
+file_handler.setFormatter(file_formatter)
+
+logger = logging.getLogger("utils")
+logger.setLevel(logging.DEBUG)
+logger.addHandler(file_handler)
 
 
 def read_file(filename=None):
     """Функция чтения файла с транзакциями."""
     if filename is None:
+        logger.error("Читаемый файл не указан.")
         return []
     try:
         with open(filename, "r", encoding="utf-8") as f:
             data = json.load(f)
+            logger.debug("Файл найден.")
 
         if not isinstance(data, list):
+            logger.error("Содержимое файла не является списком.")
             return []
-
+        logger.info("Файл прочтён.")
         return data
     except FileNotFoundError:
+        logger.error("Файл не найден.")
         print(f"Файл не найден по пути: {filename}")
         return []
     except json.JSONDecodeError:
+        logger.error("Ошибка декодирования.")
         print(f"Ошибка при декодировании JSON из файла: {filename}")
         return []
